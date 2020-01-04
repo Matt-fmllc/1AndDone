1a55a2dd6ca8f00b1db80690acfd8ed002973113
Matt-fmllc <matt.fmllc@gmail.com>
2020-01-04 15:10:13 -0800

    added in some basic components: buttons Textbox Grid CustomInput Added UI lib ( "MaterialUI" )
    Demo of components
    Email Box
    Password Box
    All Button Styles

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
+/* import logo from './logo.svg'; */
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import 'typeface-roboto';
+//import "./material-ui-dev"
+import TextField from '@material-ui/core/TextField';
+
+import Button from "./components/custombuttons/Button.js";
+import GridItem from "./components/Grid/GridItem.js"
+//import CustomInput from "./components/customInput/CustomInput.js"
+import Grid from "./components/Grid/GridContainer.js"
+
+
+
+//const useStyles = makeStyles(styles);
+
+
+function LoginControl() {
+    return (
+        <Grid container>
+            <GridItem xs={12} sm={12} md={4}>
+                <TextField
+                    label="Enter Email"
+                    id="float1"
+                    formControlProps={{
+                        fullWidth: true
+                    }}
+                />
+            </GridItem>
+            <GridItem xs={12} sm={12} md={4}>
+                <TextField
+                    label="Password"
+                    id="float2"
+                    type="password"
+                    formControlProps={{
+                        fullWidth: true
+                    }}
+                />
+            </GridItem>
+        </Grid>
+    )
 }
+
+function App() {
+    return (
+        <div className="App">
+            <LoginControl />
+            <Button type="button" color="primary">Primary</Button>
+            <Button type="button" color="info">Info</Button>
+            <Button type="button" color="success">Success</Button>
+            <Button type="button" color="danger">Danger</Button>
+            <Button type="button" color="warning">Warning</Button>
+        </div>
+
+    );
+
+}
+
+
 
 export default App;
