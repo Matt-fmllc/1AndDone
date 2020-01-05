a86044e7cc574e668fc7cd05986b7b0aebee8311
Matt-fmllc <matt.fmllc@gmail.com>
2020-01-04 19:43:26 -0800

    couple classes acting as the starting points for different components have it toggling between components via a callback to the parent component

--- src/App.js
+++ src/App.js
@@ -1,62 +1,157 @@
 import React from 'react';
-/* import logo from './logo.svg'; */
 import './App.css';
 
 import 'typeface-roboto';
-//import "./material-ui-dev"
 import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid'
 
 import Button from "./components/custombuttons/Button.js";
-import GridItem from "./components/Grid/GridItem.js"
-//import CustomInput from "./components/customInput/CustomInput.js"
-import Grid from "./components/Grid/GridContainer.js"
 
 
+class LandingControl extends React.Component {
+    constructor(props) {
+        super(props);
 
-//const useStyles = makeStyles(styles);
+        this.handleClick = this.handleClick.bind(this);
+    }
 
+    handleClick(event) {
+        const target = event.target;
+        const name = target.name;
 
-function LoginControl() {
-    return (
-        <Grid container>
-            <GridItem xs={12} sm={12} md={4}>
-                <TextField
-                    label="Enter Email"
-                    id="float1"
-                    formControlProps={{
-                        fullWidth: true
-                    }}
-                />
-            </GridItem>
-            <GridItem xs={12} sm={12} md={4}>
-                <TextField
-                    label="Password"
-                    id="float2"
-                    type="password"
-                    formControlProps={{
-                        fullWidth: true
-                    }}
-                />
-            </GridItem>
-        </Grid>
-    )
+        switch (name) {
+            case "signin":
+                this.props.setScreen(screenStates.LOGIN);
+                alert("signin pressed");
+                return;
+            case "join":
+                this.props.setScreen(screenStates.JOIN_LEAGUE);
+                alert("join pressed");
+                return;
+            default:
+                alert("unknown");
+                return;
+        }
+    }
+
+    render() {
+        return (
+            <header className="App-Body">
+                <div>
+                    <button name="signin" type="button" color="success" onClick={this.handleClick}>Sign In</button>
+                    <button name="join" type="button" color="success" onClick={this.handleClick}>Join League</button>
+                </div >
+            </header>
+        )
+    }
+}
+
+class LoginControl extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.props.setScreen(screenStates.JOIN_LEAGUE);
+    }
+
+    render() {
+        return (
+            <header className="App-Body" >
+                <div>
+                    <Grid container justify="center" spacing={1} textAlign="center" >
+                        <Grid item xs={10} sm={4}>
+                            <TextField
+                                label="Email"
+                                id="float1"
+                                size="normal"
+                                margin="dense"
+                                variant="outlined"
+                                formControlProps={{
+                                    fullWidth: true
+                                }}
+                            />
+                        </Grid>
+                        <Grid item xs={10} sm={4}>
+                            <TextField
+                                label="Password"
+                                id="float2"
+                                type="password"
+                                size="normal"
+                                margin="dense"
+                                variant="outlined"
+                                formControlProps={{
+                                    fullWidth: true
+                                }}
+                            />
+                        </Grid>
+                    </Grid>
+                    <Button type="button" color="success" onClick={this.handleClick}>Sign In</Button>
+                </div>
+            </header>
+        )
+    }
+}
+
+const screenStates = {
+    LANDING: 'landing',
+    LOGIN: 'login',
+    JOIN_LEAGUE: 'join_league',
+    CREATE_ACCOUNT: 'create_account'
+}
+
+class OneAndDone extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            xCurScreen: screenStates.LANDING,
+        };
+    }
+
+    setCurScreen = (value) => {
+        this.setState({
+            xCurScreen: value
+        })
+    }
+
+    render() {
+        const curScreen = this.state.xCurScreen;
+        switch (curScreen) {
+            
+            case screenStates.LANDING:
+                return (
+                    <LandingControl setScreen={this.setCurScreen.bind(this)}/>
+                );
+            case screenStates.LOGIN:
+                return (
+                    <LoginControl setScreen={this.setCurScreen.bind(this)}/>
+                );
+            case screenStates.JOIN_LEAGUE:
+            case screenStates.CREATE_ACCOUNT:
+            default:
+                return null;
+        }
+    }
 }
 
 function App() {
     return (
-        <div className="App">
-            <LoginControl />
-            <Button type="button" color="primary">Primary</Button>
-            <Button type="button" color="info">Info</Button>
-            <Button type="button" color="success">Success</Button>
-            <Button type="button" color="danger">Danger</Button>
-            <Button type="button" color="warning">Warning</Button>
-        </div>
-
+        <header className="App-Body">
+            <div className="OneAndDone">
+                <OneAndDone />
+            </div>
+        </header>
     );
 
 }
 
-
+//<LoginControl />
+//<Button type="button" color="primary">Primary</Button>
+//<Button type="button" color="info">Info</Button>
+//<Button type="button" color="success">Success</Button>
+//<Button type="button" color="danger">Danger</Button>
+//<Button type="button" color="warning">Warning</Button>
 
 export default App;
