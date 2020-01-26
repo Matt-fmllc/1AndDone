6ca1e2285e873571c745ec4fcf9f058e7643c7f7
Matt-fmllc <matt.fmllc@gmail.com>
2020-01-26 10:49:00 -0800

    did some cleanup work on the landing screen - realigned images - realigned buttons - abstracted some of the drawing components

--- src/components/LandingScreen.js
+++ src/components/LandingScreen.js
@@ -5,12 +5,41 @@ import React from 'react';
 import './../App.css';
 
 import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
 
 import BackGroundImage from "./../assets/images/14-Team-NFL-Playoffs.jpg";
 //import BannerImage from "./../assets/images/one-and-done-series-banner-3.jpg"
 import BannerImage from "./../assets/images/one-and-done-typewriter.jpg"
 
 import { screenStates } from "./../App.js";
+
+
+function DrawButtons(props) {
+    return (
+        <React.Fragment>
+            <Button
+                name="signin"
+                variant="contained"
+                color="secondary"
+                type="button"
+                style={{ position: 'absolute', left: '30%', top: '70%', width:'125px' }}
+                onClick={() => props.handleClick("1")}
+            >
+                Sign In
+            </Button>
+            <Button
+                name="join"
+                variant="contained"
+                color="secondary"
+                type="button"
+                style={{ position: 'absolute', left: '55%', top: '70%', width:'125px' }}
+                onClick={() => props.handleClick("2")}
+            >
+                Join League
+            </Button>
+        </React.Fragment>
+    );
+}
 
 
 export default class LandingControl extends React.Component {
@@ -34,50 +63,31 @@ export default class LandingControl extends React.Component {
         }
     }
 
-//                <div class="App-Body">
-    render() {
+   render() {
+        const screenWidth = window.innerWidth;
+
         return (
-            <div>
-                <div >
-                    <img src={BannerImage} alt=""
-                        width="450"
-                        height="100"
-                        style={{
-                            position: 'relative',
-                            verticalAlign: 'center',
-                        }}
-                    />
-                    <div class="landing_container" >
-                        <img src={BackGroundImage} alt=""
-                            width="400"
-                            style={{
-                                position: 'relative',
-                                verticalAlign: 'center',
-                            }}
-                        />
-                        <Button 
-                            name="signin"
-                            variant="contained"
-                            color="secondary"
-                            type="button"
-                            style={{ position: 'absolute', left: '25%', top: '90%' }}
-                            onClick={() => this.handleClick("1")}
-                            >
-                            Sign In
-                        </Button>
-                        <Button 
-                            name="join"
-                            variant="contained"
-                            color="secondary"
-                            type="button"
-                            style={{ position: 'absolute', left: '50%', top: '90%' }}
-                            onClick={()=>this.handleClick("2")}
-                            >
-                            Join League
-                        </Button>
-                    </div >
-                </div>
-            </div>
+            <Paper >
+                <img src={BannerImage} alt=""
+                    width="100%"
+                    height="100"
+                    top="0"
+                    left="0"
+                    style={{
+                        position: 'relative',
+                        verticalAlign: 'center',
+                    }}
+                />
+                <img src={BackGroundImage} alt=""
+                    width={screenWidth}
+                    height="100%"
+                    style={{
+                        position: 'relative',
+                        verticalAlign: 'center',
+                    }}
+                />
+                <DrawButtons handleClick={this.handleClick.bind(this)}/>
+            </Paper>
         )
     }
 }
